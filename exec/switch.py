28340b9e296527fccfce3acac6bdc799d3344718
Éric Massé <Ericmas001@gmail.com>
2018-12-04 17:25:47 -0500

    Update switch.py

--- exec/switch.py
+++ exec/switch.py
@@ -28,18 +28,18 @@ Console.WriteLine("[{0}] Taking control of gpios",
                   datetime.today().strftime("%Y-%m-%d %H:%M:%S"))
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(app_config.pin_on, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(app_config.pin_off, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(app_config.pin_on, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(app_config.pin_off, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
     input_green = GPIO.input(app_config.pin_on)
-    if input_green == False:
+    if input_green == True:
         Console.WriteLine("[{0}] Button ON Pressed",
                          datetime.today().strftime("%Y-%m-%d %H:%M:%S"))
         urllib2.urlopen(app_config.base_url + "/hvac/on/" + app_config.api_key + "/").read()
         time.sleep(0.2)
     input_black = GPIO.input(app_config.pin_off)
-    if input_black == False:
+    if input_black == True:
         Console.WriteLine("[{0}] Button OFF Pressed",
                          datetime.today().strftime("%Y-%m-%d %H:%M:%S"))
         urllib2.urlopen(app_config.base_url + "/hvac/off/" + app_config.api_key + "/").read()
